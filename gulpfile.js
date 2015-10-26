0127c59dfd6b1b997fb9583c0259a2f567860e1b
AlanLes <a.leszczuk94@gmail.com>
2015-10-26 16:09:24 +0100

    part 1 done #1

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
-//gulp
+// gulp
 var gulp = require('gulp');
 
-//plugins
+// plugins
 var connect = require('gulp-connect');
 var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglify');
-var mainifyCSS = require('gulp-mainify-css');
+var minifyCSS = require('gulp-minify-css');
 var clean = require('gulp-clean');
 
-//tasks
+// tasks
 gulp.task('lint', function() {
   gulp.src(['./app/**/*.js', '!./app/bower_components/**'])
     .pipe(jshint())
@@ -16,8 +16,8 @@ gulp.task('lint', function() {
     .pipe(jshint.reporter('fail'));
 });
 gulp.task('clean', function() {
-  gulp.src('./dist/*')
-    .pipe(clean({force: true}));
+    gulp.src('./dist/*')
+      .pipe(clean({force: true}));
 });
 gulp.task('minify-css', function() {
   var opts = {comments:true,spare:true};
@@ -28,37 +28,38 @@ gulp.task('minify-css', function() {
 gulp.task('minify-js', function() {
   gulp.src(['./app/**/*.js', '!./app/bower_components/**'])
     .pipe(uglify({
-      //inSourceMap:
-      //outSourceMap: "app.js.map"
+      // inSourceMap:
+      // outSourceMap: "app.js.map"
     }))
     .pipe(gulp.dest('./dist/'))
 });
-gulp.task('copy-bower-components', function() {
+gulp.task('copy-bower-components', function () {
   gulp.src('./app/bower_components/**')
     .pipe(gulp.dest('dist/bower_components'));
 });
-gulp.task('copy-html-files', function() {
+gulp.task('copy-html-files', function () {
   gulp.src('./app/**/*.html')
     .pipe(gulp.dest('dist/'));
 });
-gulp.task('connect', function(){
+gulp.task('connect', function () {
   connect.server({
     root: 'app/',
     port: 8888
   });
 });
-gulp.task('connectDisc', function() {
+gulp.task('connectDist', function () {
   connect.server({
     root: 'dist/',
     port: 9999
   });
 });
 
-//default task
+
+// default task
 gulp.task('default',
   ['lint', 'connect']
 );
-//build task
+// build task
 gulp.task('build',
   ['lint', 'minify-css', 'minify-js', 'copy-html-files', 'copy-bower-components', 'connectDist']
 );
