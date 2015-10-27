2bc83d17fe4a4df562764deea4cc8e3b131f9032
AlanLes <a.leszczuk94@gmail.com>
2015-10-27 14:18:54 +0100

    runSequent doesn't work properly

--- gulpfile.js
+++ gulpfile.js
@@ -9,13 +9,37 @@ var minifyCSS = require('gulp-minify-css');
 var clean = require('gulp-clean');
 var browserify = require('gulp-browserify');
 var concat = require('gulp-concat');
+var runSequence = require('run-sequence');
 var less = require('gulp-less');
 var watch = require('gulp-watch');
 
 // TASKS !
 
-// default task
-gulp.task('default', ['lint', 'browserify', 'connect', 'compile-less', 'watch-less']);
+// clean task
+gulp.task('clean', function() {
+    gulp.src('./dist/*')
+      .pipe(clean({force: true}))
+    gulp.src('./app/js/bundled.js')
+      .pipe(clean({force: true}))
+});
+
+// *** default task *** //
+gulp.task('default', function(done) {
+  runSequence('clean', 'browserify', 'connect', 'compile-less', 'watch-less', 'lint', function() {
+    console.log('default end.');
+    done();
+  });
+});
+
+
+// build task
+gulp.task('build', function(done) {
+  runSequence('clean', 'minify-css', 'browserifyDist', 'copy-html-files', 'copy-bower-components', 'connectDist', 'lint', function(){
+    console.log('build end.');
+    done();
+  });
+});
+
 
 /* Task to compile less */
 gulp.task('compile-less', function() {  
@@ -30,21 +54,19 @@ gulp.task('watch-less', function() {
 });
 
 
+
 gulp.task('lint', function() {
   gulp.src(['./app/**/*.js', '!./app/bower_components/**'])
     .pipe(jshint())
     .pipe(jshint.reporter('default'))
     .pipe(jshint.reporter('fail'));
 });
-gulp.task('clean', function() {
-    gulp.src('./dist/*')
-      .pipe(clean({force: true}));
-});
+
 gulp.task('minify-css', function() {
   var opts = {comments:true,spare:true};
   gulp.src(['./app/**/*.css', '!./app/bower_components/**'])
     .pipe(minifyCSS(opts))
-    .pipe(gulp.dest('./dist/'))
+    .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('minify-js', function() {
@@ -53,33 +75,32 @@ gulp.task('minify-js', function() {
       // inSourceMap:
       // outSourceMap: "app.js.map"
     }))
-    .pipe(gulp.dest('./dist/'))
+    .pipe(gulp.dest('./dist/'));
 });
+
 gulp.task('copy-bower-components', function () {
   gulp.src('./app/bower_components/**')
     .pipe(gulp.dest('dist/bower_components'));
 });
+
 gulp.task('copy-html-files', function () {
   gulp.src('./app/**/*.html')
     .pipe(gulp.dest('dist/'));
 });
+
 gulp.task('connect', function () {
   connect.server({
     root: 'app/',
     port: 8888
   });
 });
+
 gulp.task('connectDist', function () {
   connect.server({
     root: 'dist/',
     port: 9999
   });
 });
-
-// build task
-gulp.task('build',
-  ['lint', 'minify-css', 'browserifyDist', 'copy-html-files', 'copy-bower-components', 'connectDist']
-);
 
 gulp.task('browserify', function() {
   gulp.src(['app/js/main.js'])
@@ -89,13 +110,6 @@ gulp.task('browserify', function() {
   }))
   .pipe(concat('bundled.js'))
   .pipe(gulp.dest('./app/js'))
-});
-
-gulp.task('clean', function() {
-    gulp.src('./dist/*')
-      .pipe(clean({force: true}));
-    gulp.src('./app/js/bundled.js')
-      .pipe(clean({force: true}));
 });
 
 gulp.task('browserifyDist', function() {
