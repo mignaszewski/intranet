142f1307dcdfeba4f9c2e605437406a02e710de1
AlanLes <a.leszczuk94@gmail.com>
2015-11-04 14:57:34 +0100

    news in progress #5

--- app/js/controllers/newsctrl.js
+++ app/js/controllers/newsctrl.js
@@ -1,3 +1,24 @@
 module.exports = function($scope){
   $scope.tekst = "KACZKA";
+
+  $scope.latestNews = [
+  {
+    id: 1,
+    tittle: 'Drużyna Opery',
+    description: 'Od 01.09.2015 rozpoczęła pracę Drużyna Opery J - projekt testerski dla Opery Software. Project room znajduje się w budynku przy Ostrowskiego 9 (dawny pokój Business Development). A oto i oni: Od lewej Iga Bugaj, Alina Bugaj, Lars Jarek, Tomek Gleń, Damian Nowakowski, Igor Dlugosh oraz Daniel Dziuba. Witamy w drużynie!',
+    img: ['./img/temp_news/druzyna.jpg']
+  },
+  {
+    id: 2,
+    tittle: 'Nowy Project Manager',
+    description: 'Cześć! W gronie naszych Project Managerów witamy Tomasza Odrzywołka. Tomka znajdziemy pod mailem  tomasz.odrzywolek@altimi.com oraz pod numerem telefonu +48 534-190-316 Tomkowi życzymy wielu sukcesów oraz satysfakcji z pracy w naszej firmie.',
+    img: ['./img/temp_news/Tomasz.jpg']
+  },
+  {
+    id: 3,
+    tittle: 'Już za tydzień!!!',
+    description: '',
+    img: ['./img/temp_news/ZAPROSZENIE2.jpg']
+  }
+  ];
 };
