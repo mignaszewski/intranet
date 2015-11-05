cf11f289b5ebf68115978a82ae76371254752d3a
AlanLes <a.leszczuk94@gmail.com>
2015-11-05 15:56:10 +0100

    need to be checked #43

---
+++ app/js/controllers/announcementsctrl.js
@@ -0,0 +1,91 @@
+module.exports = function($scope) {
+  // $scope.sometext = "OGŁOSZENIA DROBNE :D";
+  $scope.announcements = announcements;
+};
+
+var announcements = [
+{
+  id: 1,
+  author: 'Aneta',
+  description: 'Zapraszam na imprezę urodzinową 21 marca 2016 roku! Będzie grubo!',
+  date: '2016.03.07'
+},
+{
+  id: 2,
+  author: 'Alan',
+  description: 'Sprzedam opla! NIEMIEC PŁAKAŁ JAK SPRZEDAWAŁ! WARTO! SUPER STAN, ŚWIETNA OKAZJA!',
+  date: '2015.11.03'
+},
+{
+  id: 3,
+  author: 'Ktoś',
+  description: 'Quasi distinctio repellat, quisquam modi omnis quam explicabo suscipit, labore ipsum assumenda beatae nisi delectus atque aliquam, iusto facilis. Modi, qui, atque!',
+  date: '2014.01.02'
+},
+{
+  id: 4,
+  author: 'Ktoś2',
+  description: 'Voluptatum, eum beatae! Iste nam dolore rerum, deleniti saepe soluta temporibus maiores nostrum sapiente odit vitae quas suscipit nulla obcaecati sit impedit!',
+  date: '2016.09.12'
+},
+{
+  id: 5,
+  author: 'Ktoś3',
+  description: 'Nam consequatur sunt id eos similique, ipsa dolor odio eveniet quas, quaerat facilis quae pariatur dolorum cum.!',
+  date: '2016.01.30'
+},
+{
+  id: 6,
+  author: 'Ktoś4',
+  description: 'In tenetur vitae repudiandae odit doloribus quas quae velit nam voluptatum.Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+  date: '2016.02.26'
+},
+{
+  id: 7,
+  author: 'Ktoś5',
+  description: 'Repellendus esse minima maxime quidem cum unde ex quaerat similique aliquam eaque harum dicta non, sapiente molestiae, magni, et nostrum voluptatum ad.',
+  date: '2015.08.04'
+},
+{
+  id: 1,
+  author: 'Aneta',
+  description: 'Zapraszam na imprezę urodzinową 21 marca 2016 roku! Będzie grubo!',
+  date: '2016.03.07'
+},
+{
+  id: 2,
+  author: 'Alan',
+  description: 'Sprzedam opla! NIEMIEC PŁAKAŁ JAK SPRZEDAWAŁ! WARTO! SUPER STAN, ŚWIETNA OKAZJA!',
+  date: '2015.11.03'
+},
+{
+  id: 3,
+  author: 'Ktoś',
+  description: 'Quasi distinctio repellat, quisquam modi omnis quam explicabo suscipit, labore ipsum assumenda beatae nisi delectus atque aliquam, iusto facilis. Modi, qui, atque!',
+  date: '2014.01.02'
+},
+{
+  id: 4,
+  author: 'Ktoś2',
+  description: 'Voluptatum, eum beatae! Iste nam dolore rerum, deleniti saepe soluta temporibus maiores nostrum sapiente odit vitae quas suscipit nulla obcaecati sit impedit!',
+  date: '2016.09.12'
+},
+{
+  id: 5,
+  author: 'Ktoś3',
+  description: 'Nam consequatur sunt id eos similique, ipsa dolor odio eveniet quas, quaerat facilis quae pariatur dolorum cum.!',
+  date: '2016.01.30'
+},
+{
+  id: 6,
+  author: 'Ktoś4',
+  description: 'In tenetur vitae repudiandae odit doloribus quas quae velit nam voluptatum.Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+  date: '2016.02.26'
+},
+{
+  id: 7,
+  author: 'Ktoś5',
+  description: 'Repellendus esse minima maxime quidem cum unde ex quaerat similique aliquam eaque harum dicta non, sapiente molestiae, magni, et nostrum voluptatum ad.',
+  date: '2015.08.04'
+}
+];
