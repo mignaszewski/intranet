c6a348763edc25f70a311a0bfc9befb7909b7992
AlanLes <a.leszczuk94@gmail.com>
2015-11-06 11:25:46 +0100

    pinboard at background, limit of signs into content of announcement

--- app/js/controllers/announcementsctrl.js
+++ app/js/controllers/announcementsctrl.js
@@ -3,11 +3,12 @@ module.exports = function($scope) {
   $scope.announcements = announcements;
 };
 
+// WPROWADZIC OGRANICZENIE, NIE WIECEJ NIŻ 130 ZNAKÓW W OPISIE OGŁOSZENIA :)
 var announcements = [
 {
   id: 1,
   author: 'Aneta',
-  description: 'Zapraszam na imprezę urodzinową 21 marca 2016 roku! Będzie grubo!',
+  description: 'Zapraszam na imprezę urodzinową 21 marca 2016 roku! Będzie grubo! Modi, qui, atque! Modi, qui, atque!',
   date: '2016.03.07'
 },
 {
@@ -18,73 +19,115 @@ var announcements = [
 },
 {
   id: 3,
-  author: 'Ktoś',
-  description: 'Quasi distinctio repellat, quisquam modi omnis quam explicabo suscipit, labore ipsum assumenda beatae nisi delectus atque aliquam, iusto facilis. Modi, qui, atque!',
+  author: 'Julian',
+  description: 'Quasi distinctio repellat, quisquam modi omnis quam explicabo suscipit, labore ipsum assumenda beatae nisi delectus atque aliquam, iusto facilis.',
   date: '2014.01.02'
 },
 {
   id: 4,
-  author: 'Ktoś2',
-  description: 'Voluptatum, eum beatae! Iste nam dolore rerum, deleniti saepe soluta temporibus maiores nostrum sapiente odit vitae quas suscipit nulla obcaecati sit impedit!',
+  author: 'Michał',
+  description: 'Voluptatum, eum beatae! Iste nam dolore rerum, deleniti saepe soluta temporibus maiores nostrum sapiente odit vitae quas suscipit nulla.',
   date: '2016.09.12'
 },
 {
   id: 5,
-  author: 'Ktoś3',
+  author: 'Bogdan',
   description: 'Nam consequatur sunt id eos similique, ipsa dolor odio eveniet quas, quaerat facilis quae pariatur dolorum cum.!',
-  date: '2016.01.30'
+  date: '2016.04.30'
 },
 {
   id: 6,
-  author: 'Ktoś4',
+  author: 'Sławomir',
   description: 'In tenetur vitae repudiandae odit doloribus quas quae velit nam voluptatum.Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-  date: '2016.02.26'
+  date: '2016.05.26'
 },
 {
   id: 7,
-  author: 'Ktoś5',
+  author: 'AgaAta',
   description: 'Repellendus esse minima maxime quidem cum unde ex quaerat similique aliquam eaque harum dicta non, sapiente molestiae, magni, et nostrum voluptatum ad.',
   date: '2015.08.04'
 },
 {
-  id: 1,
+  id: 8,
   author: 'Aneta',
-  description: 'Zapraszam na imprezę urodzinową 21 marca 2016 roku! Będzie grubo!',
-  date: '2016.03.07'
+  description: 'Zapraszam na imprezę urodzinową 21 marca 2016 roku! Będzie grubo! Iste nam dolore rerum, deleniti saepe soluta temporibus maiores nostrum.',
+  date: '2016.07.07'
 },
 {
-  id: 2,
+  id: 9,
   author: 'Alan',
-  description: 'Sprzedam opla! NIEMIEC PŁAKAŁ JAK SPRZEDAWAŁ! WARTO! SUPER STAN, ŚWIETNA OKAZJA!',
-  date: '2015.11.03'
+  description: 'Sprzedam opla! NIEMIEC PŁAKAŁ JAK SPRZEDAWAŁ! WARTO! SUPER STAN, ŚWIETNA OKAZJA! Modi, qui, atque!',
+  date: '2015.12.03'
 },
 {
   id: 3,
-  author: 'Ktoś',
-  description: 'Quasi distinctio repellat, quisquam modi omnis quam explicabo suscipit, labore ipsum assumenda beatae nisi delectus atque aliquam, iusto facilis. Modi, qui, atque!',
-  date: '2014.01.02'
+  author: 'Julianna',
+  description: 'Quasi distinctio repellat, quisquam modi omnis quam explicabo suscipit, labore ipsum assumenda beatae nisi delectus atque aliquam.',
+  date: '2014.02.02'
 },
 {
   id: 4,
-  author: 'Ktoś2',
-  description: 'Voluptatum, eum beatae! Iste nam dolore rerum, deleniti saepe soluta temporibus maiores nostrum sapiente odit vitae quas suscipit nulla obcaecati sit impedit!',
-  date: '2016.09.12'
+  author: 'Michał',
+  description: 'Voluptatum, eum beatae! Iste nam dolore rerum, deleniti saepe soluta temporibus maiores nostrum sapiente odit vitae quas suscipit nulla obcaecati sit impedit! Modi, qui, atque!',
+  date: '2016.05.12'
 },
 {
   id: 5,
-  author: 'Ktoś3',
-  description: 'Nam consequatur sunt id eos similique, ipsa dolor odio eveniet quas, quaerat facilis quae pariatur dolorum cum.!',
-  date: '2016.01.30'
+  author: 'Bogdan',
+  description: 'Nam consequatur sunt id eos similique, ipsa dolor odio eveniet quas, quaerat facilis quae pariatur dolorum cum.!Iste nam dolore rerum.',
+  date: '2016.07.30'
 },
 {
   id: 6,
-  author: 'Ktoś4',
-  description: 'In tenetur vitae repudiandae odit doloribus quas quae velit nam voluptatum.Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+  author: 'Agha',
+  description: 'In tenetur vitae repudiandae odit doloribus quas quae velit nam voluptatum.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi, qui, atque!',
+  date: '2016.09.26'
+},
+{
+  id: 7,
+  author: 'AgaAta',
+  description: 'Repellendus esse minima maxime quidem cum unde ex quaerat similique aliquam eaque harum dicta non, sapiente molestiae, magni, et nostrum voluptatum ad.',
+  date: '2015.12.04'
+},
+{
+  id: 8,
+  author: 'Aneta',
+  description: 'Zapraszam na imprezę urodzinową 21 marca 2016 roku! Będzie grubo! Iste nam dolore rerum, deleniti saepe soluta temporibus maiores nostrum.',
+  date: '2016.01.07'
+},
+{
+  id: 9,
+  author: 'Alan',
+  description: 'Sprzedam opla! NIEMIEC PŁAKAŁ JAK SPRZEDAWAŁ! WARTO! SUPER STAN, ŚWIETNA OKAZJA! Modi, qui, atque!',
+  date: '2015.05.03'
+},
+{
+  id: 3,
+  author: 'Julo',
+  description: 'Quasi distinctio repellat, quisquam modi omnis quam explicabo suscipit, labore ipsum assumenda beatae nisi delectus atque aliquam.',
+  date: '2013.08.02'
+},
+{
+  id: 4,
+  author: 'Maciek',
+  description: 'Voluptatum, eum beatae! Iste nam dolore rerum, deleniti saepe soluta temporibus maiores nostrum sapiente odit vitae quas suscipit nulla obcaecati sit impedit! Modi, qui, atque!',
+  date: '2015.10.12'
+},
+{
+  id: 5,
+  author: 'Sławek',
+  description: 'Nam consequatur sunt id eos similique, ipsa dolor odio eveniet quas, quaerat facilis quae pariatur dolorum cum.!Iste nam dolore rerum.',
+  date: '2015.01.30'
+},
+{
+  id: 6,
+  author: 'Aga',
+  description: 'In tenetur vitae repudiandae odit doloribus quas quae velit nam voluptatum.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi, qui, atque!',
   date: '2016.02.26'
 },
 {
   id: 7,
-  author: 'Ktoś5',
+  author: 'Julia',
   description: 'Repellendus esse minima maxime quidem cum unde ex quaerat similique aliquam eaque harum dicta non, sapiente molestiae, magni, et nostrum voluptatum ad.',
   date: '2015.08.04'
 }
