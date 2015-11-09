0ec0081fd3d93e5ffa128c5ad3c6955a4ede824d
AlanLes <a.leszczuk94@gmail.com>
2015-11-09 17:31:51 +0100

    array of objects, new files to download, temporary examples

--- app/js/controllers/downloadctrl.js
+++ app/js/controllers/downloadctrl.js
@@ -1,3 +1,26 @@
 module.exports = function($scope) {
   $scope.tekst = "down";
+
+  $scope.items = items;
 };
+
+var items = [
+{
+  id: 1,
+  name: 'Plik pierwszy',
+  description: 'Plik potrzebny do tego i tego.',
+  link: './download/plik1'
+},
+{
+  id: 2,
+  name: 'Plik drugi',
+  description: 'Wzór dokumentu na to i tamto.',
+  link: './download/plik2'
+},
+{
+  id: 3,
+  name: 'Plik trzeci',
+  description: 'Jeśli potrzebujesz tego, to ten plik na pewno Ci się przyda!',
+  link: './download/plik3'
+}
+];
