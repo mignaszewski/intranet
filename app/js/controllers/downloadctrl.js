8e4f3426636b09f41670802d71deebb24264e4c6
AlanLes <a.leszczuk94@gmail.com>
2015-11-10 11:19:58 +0100

    #40 almost finished, styles changed, search form added, pagination added, html ul changed into table

--- app/js/controllers/downloadctrl.js
+++ app/js/controllers/downloadctrl.js
@@ -8,19 +8,181 @@ var items = [
 {
   id: 1,
   name: 'Plik pierwszy',
-  description: 'Plik potrzebny do tego i tego.',
+  description: 'Plik potrzebny do tego i tego, tego i tego, tamtego i siamtego, hohohoo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic architecto autem delectus omnis eveniet consequatur repudiandae, vel ex, beatae quia amet illum deserunt dolores atque natus fuga. Aspernatur, blanditiis, et. Jeśli potrzebujesz tego, to ten plik na pewno Ci się przyda! Hic architecto autem delectus omnis eveniet consequatur repudiandae, vel ex, beatae quia amet illum deserunt dolores atque natus fuga.',
   link: './download/plik1'
 },
 {
   id: 2,
   name: 'Plik drugi',
-  description: 'Wzór dokumentu na to i tamto.',
+  description: 'Wzór dokumentu na to i tamto. Aspernatur, blanditiis, et.',
   link: './download/plik2'
 },
 {
   id: 3,
   name: 'Plik trzeci',
-  description: 'Jeśli potrzebujesz tego, to ten plik na pewno Ci się przyda!',
+  description: 'Jeśli potrzebujesz tego, to ten plik na pewno Ci się przyda! Hic architecto autem delectus omnis eveniet consequatur repudiandae, vel ex, beatae quia amet illum deserunt dolores atque natus fuga.',
+  link: './download/plik3'
+},
+{
+  id: 1,
+  name: 'Plik czwarty',
+  description: 'Plik potrzebny do tego i tego, tego i tego, tamtego i siamtego, hohohoo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic architecto autem delectus omnis eveniet consequatur repudiandae, vel ex, beatae quia amet illum deserunt dolores atque natus fuga.',
+  link: './download/plik1'
+},
+{
+  id: 2,
+  name: 'Plik dpioty',
+  description: 'Wzór dokumentu na to i tamto. Aspernatur, blanditiis, et.',
+  link: './download/plik2'
+},
+{
+  id: 3,
+  name: 'Plik szeeeeesc',
+  description: 'Jeśli potrzebujesz tego, to ten plik na pewno Ci się przyda! Hic architecto autem delectus omnis eveniet consequatur repudiandae, vel ex, beatae quia amet illum deserunt dolores atque natus fuga.',
+  link: './download/plik3'
+},
+{
+  id: 1,
+  name: 'Plik seven nation army',
+  description: 'Plik potrzebny do tego i tego, tego i tego, tamtego i siamtego, hohohoo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic architecto autem delectus omnis eveniet consequatur repudiandae, vel ex, beatae quia amet illum deserunt dolores atque natus fuga. ',
+  link: './download/plik1'
+},
+{
+  id: 2,
+  name: 'Plik ejt',
+  description: 'Wzór dokumentu na to i tamto. Aspernatur, blanditiis, et.',
+  link: './download/plik2'
+},
+{
+  id: 3,
+  name: 'Plik dziewiaty',
+  description: 'Jeśli potrzebujesz tego, to ten plik na pewno Ci się przyda! Beatae quia amet illum deserunt dolores atque natus fuga.',
+  link: './download/plik3'
+},
+{
+  id: 1,
+  name: 'Plik ten',
+  description: 'Plik potrzebny do tego i tego, tego i tego, tamtego i siamtego, hohohoo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic architecto autem delectus omnis eveniet consequatur repudiandae, vel ex, beatae quia amet illum deserunt dolores atque natus fuga.',
+  link: './download/plik1'
+},
+{
+  id: 2,
+  name: 'Plik ostatni',
+  description: 'Wzór dokumentu na to i tamto. Aspernatur, blanditiis, et.',
+  link: './download/plik2'
+},
+{
+  id: 3,
+  name: 'Pliknajnowszy',
+  description: 'Jeśli potrzebujesz tego, to ten plik na pewno Ci się przyda! Hic architecto autem delectus omnis eveniet consequatur repudiandae, vel ex, beatae quia amet illum deserunt dolores atque natus fuga. Hic architecto autem delectus omnis eveniet consequatur repudiandae, vel ex,',
+  link: './download/plik3'
+},
+{
+  id: 1,
+  name: 'dokument dwa',
+  description: 'Plik potrzebny do tego i tego, tego i tego, tamtego i siamtego, hohohoo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic architecto autem delectus omnis eveniet consequatur repudiandae, vel ex, beatae quia amet illum deserunt dolores atque natus fuga. Aspernatur, blanditiis, et.',
+  link: './download/plik1'
+},
+{
+  id: 2,
+  name: 'Dokument szesc',
+  description: 'Wzór dokumentu na to i tamto. Aspernatur, blanditiis, et. Jeśli potrzebujesz tego, to ten plik na pewno Ci się przyda!',
+  link: './download/plik2'
+},
+{
+  id: 3,
+  name: 'Plik trzeci',
+  description: 'Jeśli potrzebujesz tego, to ten dokument na pewno Ci się przyda! Hic architecto autem delectus omnis eveniet consequatur repudiandae, vel ex, beatae quia amet illum deserunt dolores atque natus fuga. Jeśli potrzebujesz tego, to ten dokument na pewno Ci się przyda! Hic architecto autem delectus omnis eveniet consequatur repudiandae, vel ex.',
+  link: './download/plik3'
+},
+{
+  id: 1,
+  name: 'dokument pierwszy',
+  description: 'dokument potrzebny do tego i tego, tego i tego, tamtego i siamtego, hohohoo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic architecto autem delectus omnis eveniet consequatur repudiandae, vel ex, beatae quia amet illum deserunt dolores atque natus fuga. Aspernatur, blanditiis, et. Jeśli potrzebujesz tego, to ten plik na pewno Ci się przyda! Hic architecto autem delectus omnis eveniet consequatur repudiandae, vel ex, beatae quia amet illum deserunt dolores atque natus fuga.',
+  link: './download/plik1'
+},
+{
+  id: 2,
+  name: 'Plik drugi',
+  description: 'Wzór dokumentu na to i tamto. Aspernatur, blanditiis, et, beatae quia amet illum deserunt dolores atque natus fuga. Aspernatur, blanditiis, et. Jeśli potrzebujesz tego, to ten plik na pewno Ci się przyda! Hic architecto autem delectus omnis eveniet consequatur repudiandae, vel ex, beatae quia amet illum deserunt dolores atque natus fuga.',
+  link: './download/plik2'
+},
+{
+  id: 3,
+  name: 'dokument trzeci',
+  description: 'Jeśli potrzebujesz tego, to ten plik na pewno Ci się przyda! Hic architecto autem delectus omnis eveniet consequatur repudiandae, vel ex, beatae quia amet illum deserunt dolores atque natus fuga, beatae quia amet illum deserunt dolores atque natus fuga. Aspernatur, blanditiis, et. Jeśli potrzebujesz tego, to ten plik na pewno Ci się przyda! Hic architecto autem delectus omnis eveniet consequatur repudiandae, vel ex, beatae quia amet illum deserunt dolores atque natus fuga.',
+  link: './download/plik3'
+},
+{
+  id: 1,
+  name: 'dokument pierwszy',
+  description: 'Plik potrzebny do tego i tego, tego i tego, tamtego i siamtego, hohohoo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic architecto autem delectus omnis eveniet consequatur repudiandae, vel ex, beatae quia amet illum deserunt dolores atque natus fuga. Aspernatur, blanditiis, et. Jeśli potrzebujesz tego, to ten plik na pewno Ci się przyda! Hic architecto autem delectus omnis eveniet consequatur repudiandae.',
+  link: './download/plik1'
+},
+{
+  id: 2,
+  name: 'dokument drugi',
+  description: 'Wzór dokumentu na to i tamto. Aspernatur, blanditiis, et.',
+  link: './download/plik2'
+},
+{
+  id: 3,
+  name: 'Plik trzeci',
+  description: 'Jeśli potrzebujesz tego, to ten plik na pewno Ci się przyda! Hic architecto autem delectus omnis eveniet consequatur repudiandae, vel ex.',
+  link: './download/plik3'
+},
+{
+  id: 1,
+  name: 'dokument pierwszy',
+  description: 'Plik potrzebny do tego i tego, tego i tego, tamtego i siamtego, hohohoo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic architecto autem delectus omnis eveniet consequatur repudiandae, vel ex, beatae quia amet illum deserunt dolores atque natus fuga.',
+  link: './download/plik1'
+},
+{
+  id: 2,
+  name: 'drugi',
+  description: 'Wzór dokumentu na to i tamto. Aspernatur, blanditiis, et. Aspernatur, blanditiis, et. Jeśli potrzebujesz tego, to ten plik na pewno Ci się przyda!',
+  link: './download/plik2'
+},
+{
+  id: 3,
+  name: 'dokument trzeci',
+  description: 'Jeśli potrzebujesz tego, to ten plik na pewno Ci się przyda! Hic architecto autem delectus omnis eveniet consequatur repudiandae, vel ex, beatae quia amet illum deserunt dolores atque natus fuga. Aspernatur, blanditiis, et. Jeśli potrzebujesz tego, to ten plik na pewno Ci się przyda!',
+  link: './download/plik3'
+},
+{
+  id: 1,
+  name: 'dokument pierwszy',
+  description: 'Plik potrzebny do tego i tego, tego i tego, tamtego i siamtego, hohohoo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic architecto autem delectus omnis eveniet consequatur repudiandae, vel ex, beatae quia amet illum deserunt dolores atque natus fuga. Hic architecto autem delectus omnis eveniet consequatur repudiandae, vel ex, beatae quia amet illum deserunt dolores atque natus fuga, blanditiis, et.',
+  link: './download/plik1'
+},
+{
+  id: 2,
+  name: 'dokument drugi',
+  description: 'Wzór dokumentu na to i tamto. Aspernatur.',
+  link: './download/plik2'
+},
+{
+  id: 3,
+  name: 'dokument trzeci',
+  description: 'Jeśli potrzebujesz tego, to ten plik na pewno Ci się przyda! Hic architecto autem delectus omnis eveniet consequatur repudiandae, vel ex, beatae quia amet illum deserunt dolores atque natus fuga. Hic architecto autem delectus omnis eveniet consequatur repudiandae, vel ex, beatae quia amet illum deserunt dolores atque natus fuga.',
+  link: './download/plik3'
+},
+{
+  id: 1,
+  name: 'dokument pierwszy',
+  description: 'Plik potrzebny do tego i tego, tego i tego, tamtego i siamtego, hohohoo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic architecto autem delectus omnis eveniet consequatur repudiandae, vel ex, beatae quia amet illum deserunt dolores atque natus fuga. Aspernatur, blanditiis, et. Jeśli potrzebujesz tego, to ten plik na pewno Ci się przyda!',
+  link: './download/plik1'
+},
+{
+  id: 2,
+  name: 'dokument drugi',
+  description: 'Wzór dokumentu na to i tamto. Aspernatur, blanditiis, et. vel ex, beatae quia amet illum deserunt dolores atque natus fuga.',
+  link: './download/plik2'
+},
+{
+  id: 3,
+  name: 'trzeci',
+  description: 'Jeśli potrzebujesz tego, to ten plik na pewno Ci się przyda! Hic architecto autem delectus omnis eveniet consequatur repudiandae.',
   link: './download/plik3'
 }
 ];
