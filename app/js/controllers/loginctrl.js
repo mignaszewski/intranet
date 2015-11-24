b4c6a8f64c6754e333e19c8d894ae0078a4c3dcb
Aneta Kałamacka <a.kalamacka@gmail.com>
2015-11-24 16:50:16 +0100

    Login panel

--- app/js/controllers/loginctrl.js
+++ app/js/controllers/loginctrl.js
@@ -1,36 +1,84 @@
 module.exports = function($scope, $location, $rootScope) {
 $scope.employees = employees;
 $scope.login = true;
-var employees = [
-  {
-    id: 15,
-    name: 'Jan Kowalski',
-    position: 'Developer',
-    projectName: 'Noname',
-    positionWeight: 13,
-    mail: 'jan.kowalski@altimi.com',
-    phone1: '',
-    phone2: '722 363 724',
-    img: './img/temp_employees/profile.jpg',
-    password: 'altimi'
-  }];
+  $scope.logged = function() {
+    return $scope.login;
+  };
+  // var employee.mail = $scope.mail;
+  // var employee.password = $scope.password;
 
-  
-  // $scope.logged = function() {
-  //   return $scope.login;
-  // };
-
-  $scope.submit = function() {
-    var mail = $scope.mail;
-    var password = $scope.password;
+  $scope.loggedIn = function(umail, upassword) {
+    console.log(umail, upassword);
     employees.forEach(function(el) {
-      if(el.mail == $scope.mail && el.password == $scope.password) {
-        confirm('Pomyślnie zalogowano');
+      if (el.mail === umail && el.password === upassword) {
         $scope.login = true;
-      } else {
-        confirm('Login i/lub hasło są niepoprawne');
-        $scope.login = false;
-      };
+        console.log('sukces');
+        confirm('Pomyślnie zalogowano');
+        // .then(function() {
+        //   $location.path('./partials/yourprofile.html')
+        // }); coś taiego?
+      } 
+      // else {
+      //   $scope.login = false;
+      //   console.log('pudło');
+      //   alert('Login i/lub hasło są niepoprawne');
+      //   continue;
+      // }
     });
   };
 };
+var employees = [
+  {
+    id: 15,
+    name: 'Aneta',
+    position: 'Intern',
+    projectName: 'Noname',
+    positionWeight: 13,
+    mail: 'aneta@altimi.com',
+    phone1: '',
+    phone2: '722 363 724',
+    img: './img/temp_employees/profile.jpg',
+    password: 'aneta'
+  },
+  {
+    id: 16,
+    name: 'Admin',
+    position: 'Admin',
+    projectName: 'Noname',
+    positionWeight: 13,
+    mail: 'admin@altimi.com',
+    phone1: '',
+    phone2: '722 363 724',
+    img: './img/temp_employees/profile.jpg',
+    password: 'admin'
+  },
+  {
+    id: 17,
+    name: 'Aneta',
+    position: 'Intern',
+    projectName: 'Noname',
+    positionWeight: 13,
+    mail: 'adrian@altimi.com',
+    phone1: '',
+    phone2: '722 363 724',
+    img: './img/temp_employees/profile.jpg',
+    password: 'adrian'
+  }];
+
+  
+  
+// module.exports = function($scope, $http, $window) {
+//   $scope.loginError = false;
+//   $scope.login = function () {
+//     $scope.loginError = false;
+//     $http.post('/auth/login', {mail: $scope.mail, password: $scope.password})
+//       .success(function (response) {
+//         $window.location='/';
+//       })
+//       .error(function (err, status) {
+//         $scope.username = '';
+//         $scope.password = '';
+//         $scope.loginError = true;
+//       })
+//     };
+//   };
