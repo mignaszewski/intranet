0b01d2eaa08dc8eeee683b54510193ceaea6202f
AlanLes <a.leszczuk94@gmail.com>
2015-11-30 13:58:07 +0100

    change the url where to access the loopbackrestapi

--- app/js/main.js
+++ app/js/main.js
@@ -41,6 +41,13 @@
     }
   ])
 
+  // ///////////////////////////////////////////////////////////////////////
+  .config(function(LoopBackResourceProvider) {
+    // Change the URL where to access the LoopBack REST API server
+    LoopBackResourceProvider.setUrlBase('http://localhost:3003');
+  })
+  // ///////////////////////////////////////////////////////////////////////
+  
   //Load controller
   .controller('MainViewController', ['$scope', mainviewCtrl])
   .controller('CalendarController', ['$scope', calendarCtrl])
